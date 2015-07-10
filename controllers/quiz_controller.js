0cc5f108e207ecfea3016d9388d12927cc921fa8
lfvicente <lfvicente@gmail.com>
2015-07-10 23:01:50 +0200

    CSS responsive

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 // GET /quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Grecia'});
+exports.question = function(req, res) {
+   res.render('quizes/question', {pregunta: 'Capital de Grecia'});
 };
 
 // GET /quizes/answer
-exports.answer = function(req, res){
-	If (req.query.respuesta === 'Atenas'){ 
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+exports.answer = function(req, res) {
+   if (req.query.respuesta === 'Atenas'){
+      res.render('quizes/answer', {respuesta: 'Correcto'});
+   } else {
+      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+   }
 };
